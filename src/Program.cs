72758c52b6e83a033daefd38297da7a00dfddbb2
iplmr <45397969+iplmr@users.noreply.github.com>
2024-04-16 11:20:27 +0300

    creat new folder Manage and new class library and test Book & User in main

--- src/Program.cs
+++ src/Program.cs
@@ -5,8 +5,16 @@ internal class Program
     private static void Main()
     {
 
+        Book book1 = new Book("Learn C#", new DateTime(2020, 1, 5));
+        Console.WriteLine("\n_______________________________________ Book Dtailes _______________________________________\n");
+        Console.WriteLine($"Book Name: {book1.Title} Created Date: {book1.createdDate} Book Id: {book1.id}");
+
+        User user1 = new User("salman", new DateTime(2024, 4, 16));
+        Console.WriteLine("\n_______________________________________ User Dtailes _______________________________________\n");
+        Console.WriteLine($"User Name: {user1.Name} Created Date: {user1.createdDate} User Id: {user1.id}\n");
+
 
     }
 
-    
+
 }
