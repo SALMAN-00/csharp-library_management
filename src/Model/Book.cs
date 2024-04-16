f44036a1613e0275c274f7332e67bce369b8cc05
iplmr <45397969+iplmr@users.noreply.github.com>
2024-04-16 10:57:12 +0300

    creat new folder Model and new class Book

---
+++ src/Model/Book.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using library;
+
+namespace library
+{
+    public class Book : BaseEntity
+    {
+        public string Title { get; set; }
+
+        public Book(string title, DateTime? createdDate = null) : base(createdDate)
+        {
+
+            Title = title;
+        }
+
+    }
+}
